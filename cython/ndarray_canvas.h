043a61b0bb5c63a2a4722eb613eb489a6f02a785
Erik Hvatum <ice.rikh@gmail.com>
2015-01-26 19:14:00 -0600

    .

--- cython/ndarray_canvas.h
+++ cython/ndarray_canvas.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // 
-// Authors: Erik Hvatum <ice.rikh@gmail.com> 
+// Authors: Erik Hvatum <ice.rikh@gmail.com>
 
 #pragma once
 
