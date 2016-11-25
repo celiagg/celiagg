0c4870226fc4e3a4a4f3fb5cfb4c537bebf9bc19
John Wiggins <jwiggins@enthought.com>
2016-11-25 23:47:05 +0100

    Only cp27 and cp35

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -13,7 +13,7 @@ make install
 popd
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/cp{27,35}*/bin; do
     ${PYBIN}/pip install -r /io/travis/build-requirements.txt
     ${PYBIN}/pip wheel /io/ -w wheelhouse/
 done
@@ -24,7 +24,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in /opt/python/cp{27,35}*/bin/; do
     ${PYBIN}/pip install celiagg --no-index -f /io/wheelhouse
     (cd $HOME; ${PYBIN}/nosetests celiagg)
 done
