9e604c8ae7d940873efa7462e9e9f0ffbdd85f02
John Wiggins <jwiggins@users.noreply.github.com>
2016-12-10 20:05:02 +0000

    Don't run twine from within docker (#18)

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -2,3 +2,7 @@
 
 # Run the build-wheels script in the docker container
 docker run --rm -v `pwd`:/io $DOCKER_IMAGE /io/travis/build-wheels.sh
+
+# Upload to PyPI
+pip install twine
+twine upload -u jwiggins wheelhouse/celiagg-*.whl
