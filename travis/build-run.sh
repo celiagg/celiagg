2d55f0906aa96c07b5113f633b255c94c48cf6b8
John Wiggins <john.wiggins@xfel.eu>
2016-12-11 00:16:44 +0100

    Undo changes made for testing

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -2,3 +2,24 @@
 
 # Run the build-wheels script in the docker container
 docker run --rm -v `pwd`:/io $DOCKER_IMAGE /io/travis/build-wheels.sh
+
+# Install conda
+URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+wget "${URL}" -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda create -q -n twine_env python=3.5 numpy cython freetype nose
+source activate twine_env
+
+# Build an SDIST
+python setup.py sdist
+
+# Upload to PyPI
+pip install twine
+twine upload -u jwiggins wheelhouse/celiagg-*.whl
+twine upload -u jwiggins dist/celiagg-*.tar.gz
