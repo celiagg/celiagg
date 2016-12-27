7a88b4b6beb4f5d51440385dcd062cf4e0fcbc89
John Wiggins <john.wiggins@xfel.eu>
2016-12-27 15:00:26 +0100

    Add a sky and tweak colors

--- examples/terrain.py
+++ examples/terrain.py
@@ -13,18 +13,18 @@ def draw_horizon(xs, ys, canvas, gs, fill):
     path.line_to(xs[0], ys[0])
     for x, y in zip(xs[1:], ys[1:]):
         path.line_to(x, y)
-    path.line_to(size[0], 0)
+    path.line_to(xs[-1], 0)
     path.close()
 
     transform = agg.Transform()
     canvas.draw_shape(path, transform, gs, fill=fill)
 
 
-def gen_horizon(numpoints, edgemax):
-    xs = np.linspace(0, size[0], numpoints, endpoint=True)
+def gen_horizon(numpoints, xmax, ymax):
+    xs = np.linspace(0, xmax, numpoints, endpoint=True)
     ys = np.zeros(numpoints)
-    ys[0] = np.random.rand(1) * edgemax
-    ys[-1] = np.random.rand(1) * edgemax
+    ys[0] = np.random.rand(1) * ymax
+    ys[-1] = np.random.rand(1) * ymax
     midpoint_displace(xs, ys, 0, numpoints-1)
     return xs, ys
 
@@ -55,7 +55,7 @@ def midpoint_displace(xs, ys, index0, index1):
 
     midindex = (index0 + index1) // 2
     xdist = xs[index1] - xs[index0]
-    displace = (0.5 - np.random.rand(1)) * xdist
+    displace = randneghalf2half() * xdist
     ys[midindex] = (ys[index0] + ys[index1]) / 2 + displace
 
     midpoint_displace(xs, ys, index0, midindex)
@@ -70,18 +70,48 @@ def nearest_pow2(value):
     return 2**count
 
 
+def randneghalf2half():
+    return (0.5 - np.random.rand(1)[0])
+
+
+def sky(width, height, canvas, gs):
+    transform = agg.Transform()
+    star_shape = agg.Path()
+    angles = np.linspace(0, 2*np.pi, num=5, endpoint=False)
+    pts = np.stack([np.cos(angles), np.sin(angles)]).T * 7
+    star_shape.lines([pts[i] for i in (0, 2, 4, 1, 3)])
+    star_shape.close()
+
+    star_count = 40
+    xs = np.random.rand(star_count) * width
+    ys = np.random.rand(star_count) * height
+
+    r, g, b = hsv2rgb(.165, .96, .99)
+    paint = agg.SolidPaint(r, g, b, .66)
+
+    canvas.clear(0, 0, 0)
+    for x, y in zip(xs, ys):
+        transform.reset()
+        transform.translate(x, y)
+        canvas.draw_shape(star_shape, transform, gs, fill=paint)
+
+
 def terrain(size):
     canvas = agg.CanvasRGB24(np.empty((size[1], size[0], 3), dtype='uint8'),
                              bottom_up=True)
     gs = agg.GraphicsState(drawing_mode=agg.DrawingMode.DrawFill)
-    canvas.clear(0, 0, 0)
+
+    sky(size[0], size[1], canvas, gs)
 
     numpoints = nearest_pow2(size[0] // 4) + 1
     for div in range(2, 9):
-        xs, ys = gen_horizon(numpoints, size[1] / (div - 1))
-        frac = (div - 1) / 8
-        r, g, b = hsv2rgb(frac, .7, .7)
-        fill = agg.SolidPaint(r, g, b, 0.5 * (1-frac) + 0.25)
+        alpha = 1.0 - ((div - 2) / 8)
+        hue = (div - 1) / 8 + randneghalf2half() * .125
+        sat = .6 + randneghalf2half() * .4
+        val = .6 + randneghalf2half() * .4
+        r, g, b = hsv2rgb(hue, sat, val)
+        fill = agg.SolidPaint(r, g, b, alpha)
+        xs, ys = gen_horizon(numpoints, size[0], size[1] / (div - 1))
         ys /= div
         draw_horizon(xs, ys, canvas, gs, fill)
 
