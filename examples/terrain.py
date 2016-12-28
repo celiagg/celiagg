210128cb2d924771b01d58f8bcbed03d8d2e9663
John Wiggins <john.wiggins@xfel.eu>
2016-12-28 22:58:18 +0100

    More terrain tweaks

--- examples/terrain.py
+++ examples/terrain.py
@@ -7,16 +7,25 @@ from skimage.io import imsave
 import celiagg as agg
 
 
-def draw_horizon(xs, ys, canvas, gs, fill):
+def draw_horizon(xs, ys, canvas, gs):
+    def rcolor():
+        return hsv2rgb(.125 + randneghalf2half() * .25,
+                       .6 + randneghalf2half() * .4,
+                       .6 + randneghalf2half() * .4)
+
+    width = xs[-1]
     path = agg.Path()
     path.move_to(0, 0)
     path.line_to(xs[0], ys[0])
     for x, y in zip(xs[1:], ys[1:]):
         path.line_to(x, y)
-    path.line_to(xs[-1], 0)
+    path.line_to(width, 0)
     path.close()
 
     transform = agg.Transform()
+    fill = agg.LinearGradientPaint(0, 0, width, 0, randgradstops(2, rcolor),
+                                   agg.GradientSpread.SpreadReflect,
+                                   agg.GradientUnits.UserSpace)
     canvas.draw_shape(path, transform, gs, fill=fill)
 
 
@@ -29,23 +38,23 @@ def gen_horizon(numpoints, xmax, ymax):
     return xs, ys
 
 
-def hsv2rgb(h, s, v):
+def hsv2rgb(h, s, v, a=1.0):
     hp = h * 6
     c = s * v
     x = c * (1 - abs(hp % 2 - 1))
     if 0. <= hp <= 1.:
-        return c, x, 0.
+        return c, x, 0., a
     elif 1. < hp <= 2.:
-        return x, c, 0
+        return x, c, 0, a
     elif 2. < hp <= 3.:
-        return 0, c, x
+        return 0, c, x, a
     elif 3. < hp <= 4.:
-        return 0, x, c
+        return 0, x, c, a
     elif 4. < hp <= 5.:
-        return x, 0, c
+        return x, 0, c, a
     elif 5. < hp <= 6.:
-        return c, 0, x
-    return 0, 0, 0
+        return c, 0, x, a
+    return 0, 0, 0, a
 
 
 def midpoint_displace(xs, ys, index0, index1):
@@ -70,30 +79,43 @@ def nearest_pow2(value):
     return 2**count
 
 
+def randgradstops(count, cfunc):
+    return [(off,) + cfunc() for off in np.linspace(0, 1, count)]
+
+
 def randneghalf2half():
     return (0.5 - np.random.rand(1)[0])
 
 
 def sky(width, height, canvas, gs):
+    def rcolor():
+        return hsv2rgb(.55 + randneghalf2half() * .25,
+                       .6 + randneghalf2half() * .125,
+                       .25 + randneghalf2half() * .125)
+
     transform = agg.Transform()
-    star_shape = agg.Path()
+    starpaint = agg.SolidPaint(*hsv2rgb(.165, .96, .99))
+    starshape = agg.Path()
     angles = np.linspace(0, 2*np.pi, num=5, endpoint=False)
     pts = np.stack([np.cos(angles), np.sin(angles)]).T * 7
-    star_shape.lines([pts[i] for i in (0, 2, 4, 1, 3)])
-    star_shape.close()
+    starshape.lines([pts[i] for i in (0, 2, 4, 1, 3)])
+    starshape.close()
+
+    bgshape = agg.Path()
+    bgshape.rect(0, 0, width, height)
+    bgpaint = agg.LinearGradientPaint(0, 0, 0, height,
+                                      randgradstops(2, rcolor),
+                                      agg.GradientSpread.SpreadReflect,
+                                      agg.GradientUnits.UserSpace)
+    canvas.draw_shape(bgshape, transform, gs, fill=bgpaint)
 
     star_count = 40
     xs = np.random.rand(star_count) * width
     ys = np.random.rand(star_count) * height
-
-    r, g, b = hsv2rgb(.165, .96, .99)
-    paint = agg.SolidPaint(r, g, b, .66)
-
-    canvas.clear(0, 0, 0)
     for x, y in zip(xs, ys):
         transform.reset()
         transform.translate(x, y)
-        canvas.draw_shape(star_shape, transform, gs, fill=paint)
+        canvas.draw_shape(starshape, transform, gs, fill=starpaint)
 
 
 def terrain(size):
@@ -102,18 +124,8 @@ def terrain(size):
     gs = agg.GraphicsState(drawing_mode=agg.DrawingMode.DrawFill)
 
     sky(size[0], size[1], canvas, gs)
-
-    numpoints = nearest_pow2(size[0] // 4) + 1
-    for div in range(2, 9):
-        alpha = 1.0 - ((div - 2) / 8)
-        hue = (div - 1) / 8 + randneghalf2half() * .125
-        sat = .6 + randneghalf2half() * .4
-        val = .6 + randneghalf2half() * .4
-        r, g, b = hsv2rgb(hue, sat, val)
-        fill = agg.SolidPaint(r, g, b, alpha)
-        xs, ys = gen_horizon(numpoints, size[0], size[1] / (div - 1))
-        ys /= div
-        draw_horizon(xs, ys, canvas, gs, fill)
+    xs, ys = gen_horizon(nearest_pow2(size[0] // 4) + 1, size[0], size[1])
+    draw_horizon(xs, ys / 2, canvas, gs)
 
     imsave('terrain.png', canvas.array)
 
