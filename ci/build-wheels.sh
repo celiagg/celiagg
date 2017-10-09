ee39415cc3b35214491772dd6acdb060abcb5f48
John Wiggins <jwiggins@users.noreply.github.com>
2017-10-09 21:38:50 +0200

    Fix the release build and prep for 1.0.2 (#52)

--- ci/build-wheels.sh
+++ ci/build-wheels.sh
@@ -8,7 +8,7 @@ PACKAGES=(
 )
 
 URLS=(
-    http://zlib.net/zlib-1.2.8.tar.gz
+    https://downloads.sourceforge.net/project/libpng/zlib/1.2.8/zlib-1.2.8.tar.gz
     http://download.sourceforge.net/libpng/libpng-1.6.26.tar.gz
     http://download.savannah.gnu.org/releases/freetype/freetype-2.6.5.tar.gz
 )
