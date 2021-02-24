f53783617101ac3d9c3090d36f1a5ac9d3a32d91
John Wiggins <jwiggins@enthought.com>
2021-02-24 16:08:15 +0100

    Add some text rendering tests

--- celiagg/tests/test_text.py
+++ celiagg/tests/test_text.py
@@ -1,10 +1,31 @@
 from __future__ import unicode_literals
 
+import os
+import sys
 import unittest
 
 import numpy as np
 
 import celiagg as agg
+
+
+def _get_a_font():
+    """ Attempt to find a font file to load
+    """
+    if sys.platform in ('win32', 'cygwin'):
+        directory = r'C:\Windows\Fonts'
+    elif sys.platform == 'darwin':
+        directory = '/System/Library/Fonts'
+    else:
+        directory = '/usr/share/fonts'
+
+    for name in os.listdir(directory):
+        if not name.lower().endswith('.ttf'):
+            continue
+
+        return os.path.join(directory, name)
+
+    return 'Mojibake'
 
 
 class TestTextDrawing(unittest.TestCase):
@@ -31,3 +52,52 @@ class TestTextDrawing(unittest.TestCase):
         canvas.draw_text(text_byte, font_unicode, transform, gs)
         canvas.draw_text(text_unicode, font_byte, transform, gs)
         canvas.draw_text(text_byte, font_byte, transform, gs)
+
+    def test_text_rendering(self):
+        font_cache = agg.FontCache()
+        canvas = agg.CanvasRGB24(
+            np.zeros((100, 100, 3), dtype=np.uint8), font_cache=font_cache,
+        )
+        canvas.clear(1.0, 1.0, 1.0)
+
+        # This loads a random font
+        font_name = _get_a_font()
+        font = agg.Font(font_name, 24.0, agg.FontCacheType.RasterFontCache)
+        gs = agg.GraphicsState()
+        paint = agg.SolidPaint(1.0, 0.0, 0.0, 1.0)
+        transform = agg.Transform()
+        transform.translate(25, 50)
+
+        text = 'Hello!'
+        canvas.draw_text(text, font, transform, gs, stroke=paint, fill=paint)
+
+        check = np.sum(canvas.array == [255, 0, 0], axis=2) == 3
+        self.assertTrue(check.any())
+
+    def test_text_measurement(self):
+        # This is a regression test for issue #62
+        # https://github.com/celiagg/celiagg/issues/62
+
+        font_cache = agg.FontCache()
+        canvas = agg.CanvasRGB24(
+            np.zeros((100, 100, 3), dtype=np.uint8), font_cache=font_cache,
+        )
+        font_name = _get_a_font()
+        font = agg.Font(font_name, 12.0, agg.FontCacheType.RasterFontCache)
+        gs = agg.GraphicsState()
+        paint = agg.SolidPaint(1.0, 0.0, 0.0, 1.0)
+        text = 'Some appropriate string'
+        transform = agg.Transform()
+
+        # Measure before drawing
+        width_before = font_cache.width(font, text)
+
+        # Draw with a transform
+        transform.translate(25, 75)
+        transform.rotate(0.75)
+        canvas.draw_text(text, font, transform, gs, stroke=paint, fill=paint)
+
+        # Measure after drawing
+        width_after = font_cache.width(font, text)
+
+        self.assertEqual(width_after, width_before)
