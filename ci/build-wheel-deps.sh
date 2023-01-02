66816bbd660b7b312245c3e3389ee190af012cec
John Wiggins <plasticjeebus@gmail.com>
2023-01-02 16:31:42 +0100

    old harfbuzz again

--- ci/build-wheel-deps.sh
+++ ci/build-wheel-deps.sh
@@ -4,17 +4,15 @@ set -e -x
 PACKAGES=(
     libpng-1.6.37
     freetype-2.12.1
-    harfbuzz-3.2.0
+    harfbuzz-2.6.8
 )
 
 URLS=(
     https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz
     https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.gz
-    https://github.com/harfbuzz/harfbuzz/archive/refs/tags/3.2.0.tar.gz
+    https://github.com/harfbuzz/harfbuzz/archive/refs/tags/2.6.8.tar.gz
 )
 
-# XXX: Not sure if this will work
-pip install --no-cache-dir meson==0.56.0
 
 for idx in {0..2}; do
     PACK=${PACKAGES[$idx]}
@@ -22,13 +20,8 @@ for idx in {0..2}; do
     curl -L "${URL}" -o ${PACK}.tar.gz
     tar zxf ${PACK}.tar.gz
     pushd ${PACK}
-    if [ "$idx" -eq "2" ]; then
-        # Harfbuzz is special
-        meson build
-    else
-        ./configure
-        make
-        make install
-    fi
+    ./configure
+    make
+    make install
     popd
 done
