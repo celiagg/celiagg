3558605a649faa283e76d733aa8f866c525fccd4
John Wiggins <plasticjeebus@gmail.com>
2023-01-02 15:26:32 +0100

    TEST: Debug the wheel build

--- ci/build-wheel-deps.sh
+++ ci/build-wheel-deps.sh
@@ -5,14 +5,14 @@ PACKAGES=(
     zlib-1.2.11
     libpng-1.6.37
     freetype-2.10.4
-    harfbuzz-2.6.4
+    harfbuzz-2.6.8
 )
 
 URLS=(
-    https://www.zlib.net/zlib-1.2.11.tar.gz
+    https://zlib.net/fossils/zlib-1.2.11.tar.gz
     https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz
     https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.gz
-    https://github.com/harfbuzz/harfbuzz/archive/2.6.4.tar.gz
+    https://github.com/harfbuzz/harfbuzz/archive/refs/tags/2.6.8.tar.gz
 )
 
 for idx in {0..3}; do
