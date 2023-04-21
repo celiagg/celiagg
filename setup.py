a3c12cd4b749b6943b3c2661b162e96e16a0d971
Corran Webster <cwebster@enthought.com>
2023-04-21 14:16:45 +0100

    Update changelog and bump version number.

--- setup.py
+++ setup.py
@@ -227,7 +227,7 @@ if cython_build_ext is not None:
 setup(
     name='celiagg',
     license='MIT',
-    version='2.1.3',
+    version='2.1.4',
     description='Anti-Grain Geometry for Python 3 with Cython',
     long_description=long_description,
     long_description_content_type='text/x-rst',
